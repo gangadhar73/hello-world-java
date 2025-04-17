5781eac0223babbc9ba1e140dbc16eddce78f04f
Gangadhar <thotagangadhar73@gmail.com>
2025-04-17 16:52:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,8 +35,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaDevOps");
-     System.out.println("Hello, AWSB05DevOps_AWS011");
-     System.out.println("Hello, Jenkinsteams"); 
+     System.out.println("Hello, AWSB05DevOps_vcube");
+     System.out.println("Hello, gangster"); 
 
   }
 }
